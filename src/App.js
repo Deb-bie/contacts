6bc2283f59a387efaea71f95df21500e2b9c58cc
unknown <Deb-bie>
2022-01-06 23:48:32 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Signup from './pages/signup/signup';
 import Signin from './pages/signin/signin';
 import Home from './pages/home/home.js';
+// import Books from './pages/books/books.js';
+import Add from './pages/add/add';
+import View from './pages/view/view';
+import Edit from './pages/edit/edit';
 
 
 
@@ -14,9 +18,17 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
 
+        {/* <Route exact path='/' element={<Books />} /> */}
+
         <Route path='signup' element={<Signup />} />
 
         <Route path='signin' element={<Signin />} />
+
+        <Route path='add' element={<Add />} />
+
+        <Route path='view' element={<View />} />
+
+        <Route path='/view/:id' element={<Edit />} />
       </Routes>
     </Router>
   );
